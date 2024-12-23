01ffebeb293de68fc9f5fdfde9e506a130378717
pseudonano <teguhpambudi1251@gmail.com>
2024-12-23 15:05:57 +0700

    refactor parabankautomation login-logout

--- src/paraBankAutomation/ParaBankAutomation.java
+++ src/paraBankAutomation/ParaBankAutomation.java
@@ -6,13 +6,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ParaBankAutomation {
+	private static final String BASE_URL = "https://parabank.parasoft.com/parabank/index.htm";
+	private static final By USERNAME_FIELAD = By.xpath("//input[@name='username']");
+	private static final By PASSWORD_FIELD = By.xpath("//input[@name='password']");
+	private static final By BUTTON_LOGIN = By.xpath("//input[@value='Log In']");
+	private static final By LOGOUT_LINK = By.xpath("//a[@href='logout.htm']");
+	
 public static void runParaBankAutomation(WebDriver driver) {
-	driver.get("https://parabank.parasoft.com/parabank/index.htm");
-	driver.findElement(By.xpath("//input[@name='username']")).sendKeys("john");
-	driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demo");
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	driver.findElement(By.xpath("//input[@value='Log In']")).click();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	driver.findElement(By.xpath("//a[@href='logout.htm']")).click();
+	loadPage(driver);
+	login(driver,"john","demo");
+	logout(driver);
 }
+public static void loadPage(WebDriver driver) {
+	driver.get(BASE_URL);
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 }
+
+public static void login(WebDriver driver, String username, String password) {
+	driver.findElement(USERNAME_FIELAD).sendKeys(username);
+	driver.findElement(PASSWORD_FIELD).sendKeys(password);
+	driver.findElement(BUTTON_LOGIN).click();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+}
+
+public static void logout(WebDriver driver) {
+	driver.findElement(LOGOUT_LINK).click();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+}
+
+}
+
