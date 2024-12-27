2747e0779bf0a99eba6a6561887f5cdd0068194d
pseudonano <teguhpambudi1251@gmail.com>
2024-12-28 02:01:46 +0700

    add xpath parent-child relation

--- src/seleniumClickJetDemo/SeleniumDynamicDropdown.java
+++ src/seleniumClickJetDemo/SeleniumDynamicDropdown.java
@@ -8,16 +8,11 @@ import org.openqa.selenium.WebDriver;
 public class SeleniumDynamicDropdown {
 	private static final By FROM_DROPDOWN = By.id("ctl00_mainContent_ddl_originStation1_CTXT");
 	private static final By SELECTFROM_VALUE = By.xpath("//a[@value='IXB']");
-	private static final By SELECTTO_VALUE = By.xpath("(//a[@value='BLR'][normalize-space()='Bengaluru (BLR)'])[2]");
+	private static final By IDXSELECTTO_VALUE = By.xpath("(//a[@value='BLR'][normalize-space()='Bengaluru (BLR)'])[2]");
+	private static final By RELSELECTTO_VALUE = By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BLR']");
 	
 	public static void runSeleniumDynamicDropdown(WebDriver driver) {
 		driver.get(SeleniumStaticDropdown.BASE_URL);
-//		try {
-//			Thread.sleep(Duration.ofSeconds(5));
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		driver.findElement(FROM_DROPDOWN).click();
 		driver.findElement(SELECTFROM_VALUE).click();
 		try {
@@ -26,7 +21,10 @@ public class SeleniumDynamicDropdown {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		driver.findElement(SELECTTO_VALUE).click();
+//		driver.findElement(IDXSELECTTO_VALUE).click();
+		
+		// using parent-child relationship
+		driver.findElement(RELSELECTTO_VALUE).click();
 		
 	}
 
